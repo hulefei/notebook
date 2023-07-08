946ccfe7048f210c62b243bfad1d165b2fec81ff
hulefei <hulefei29@gmail.com>
2023-07-08 23:23:15 +0800

    update linux

--- docker-run.sh
+++ docker-run.sh
@@ -2,3 +2,7 @@ docker run -e URL=$1 -e CATEGORY=$2 \
 -v /data/home/lefeihu/documents/notebook/results:/app/results  \
 -v /data/home/lefeihu/documents/notebook:/app  \
 hulefei/notebook
+
+git add .
+git commit -m "update %1"
+git push
