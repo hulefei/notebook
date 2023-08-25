4d6373abcda6916afc91f5a0295f67ce6310bbbd
hulefei <hulefei29@gmail.com>
2023-08-25 13:08:27 +0800

    update

--- zmain.bat
+++ zmain.bat
@@ -5,7 +5,7 @@ python app.py
 git add .
 git commit -m "update"
 
-git remote set-url origin https://hulefei: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@github.com/hulefei/notebook.git
+git remote set-url origin https://hulefei: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@github.com/hulefei/notebook.git
 git push origin main
 
 timeout /t 60
