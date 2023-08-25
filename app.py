246983d9a6a737bb6b076dc1905aeae57b8d26e2
hulefei <hulefei29@gmail.com>
2023-08-25 12:22:40 +0800

    update

--- app.py
+++ app.py
@@ -6,19 +6,28 @@ class IndexGenerator:
         self.template = ""
         self.index_content = ""
         self.list_category = ""
-        self.list_content = ""
+        self.list_dir = []
         self.list_article = []
         self.init()
 
     def init(self):
+        self.init_dir_list()
         self.generate_list_category()
         self.generate_article_list()
-        self.generate_list_content2()
-        self.generate_index_content()
 
     def generate(self):
         with open(f"index.html", 'w', encoding="utf-8") as f:
-            f.write(self.index_content)
+            f.write(self.generate_content(f"index"))
+
+        for dir_name in self.list_dir:
+            with open(f"{dir_name}.html", 'w', encoding="utf-8") as f:
+                f.write(self.generate_content(f"{dir_name}"))
+
+    def init_dir_list(self):
+        for dir_name in os.listdir(f"{os.getcwd()}"):
+            dir_path = f"{os.getcwd()}/{dir_name}"
+            if os.path.isdir(dir_path) and not dir_name.startswith("_") and not dir_name.startswith("."):
+                self.list_dir.append(dir_name)
 
     def generate_article_list(self):
         for dir_name in os.listdir(f"{os.getcwd()}"):
@@ -35,33 +44,33 @@ class IndexGenerator:
                         self.list_article.append(article_obj)
 
     def generate_list_category(self):
-        for dir_name in os.listdir(f"{os.getcwd()}"):
-            dir_path = f"{os.getcwd()}/{dir_name}"
-            if os.path.isdir(dir_path) and not dir_name.startswith("_") and not dir_name.startswith("."):
-                self.list_category += f"            <li><a href='/category/tech'>{dir_name}</a></li>"
-                self.list_category += "\n"
+        for dir_name in self.list_dir:
+            self.list_category += f"            <li><a href='{dir_name}.html '>{dir_name}</a></li>"
+            self.list_category += "\n"
 
-    def generate_list_content(self):
-        for article in self.list_article:
-            self.list_content += (f"<li><a href='{article['category']}/{article['title']}.html'>{article['title']}</a"
-                                  f"></li> \n")
+    @staticmethod
+    def generate_list_content(articles):
+        local_content = ""
+        for article in articles:
+            local_content += "        <article>"
+            local_content += f"<h3><a href='{article['category']}/{article['title']}.html'>{article['title']}</a></h3>"
+            local_content += "<div class='post-metadata'>"
+            local_content += f"<span class='category-tag'>{article['category']}</span>"
+            local_content += f"<p class='post-date'>发布日期: {article['date']}</p>"
+            local_content += "</div>"
+            local_content += f"</article>"
+            local_content += "\n"
+        return local_content
 
-    def generate_list_content2(self):
-        for article in self.list_article:
-            self.list_content += "        <article>"
-            self.list_content += f"<h3><a href='{article['category']}/{article['title']}.html'>{article['title']}</a></h3>"
-            self.list_content += "<div class='post-metadata'>"
-            self.list_content += f"<span class='category-tag'>{article['category']}</span>"
-            self.list_content += f"<p class='post-date'>发布日期: {article['date']}</p>"
-            self.list_content += "</div>"
-            self.list_content += f"</article>"
-            self.list_content += "\n"
-
-    def generate_index_content(self):
+    def generate_content(self, category):
+        # filter self.list_article by category
+        articles = [article for article in self.list_article if article["category"] == category or category == "index"]
         with open(f"index.template", 'r', encoding="utf-8") as f:
             self.template = f.read()
-        self.index_content = self.template.replace("%category%", self.list_category)
-        self.index_content = self.index_content.replace("%hook1%", self.list_content)
+        local_article = self.generate_list_content(articles)
+        local_content = self.template.replace("%category%", self.list_category)
+        local_content = local_content.replace("%article%", local_article)
+        return local_content
 
 
 if __name__ == "__main__":
